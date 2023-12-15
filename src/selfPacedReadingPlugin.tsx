7c41a72d6a3b1d3114ba67f1886e9446bc5adf5d
janApupa <aloha.miyashita@gmail.com>
2023-12-15 17:30:00 +0900

    SPR 用プラグインの開発を続行
    Author:    janApupa <aloha.miyashita@gmail.com>

--- src/selfPacedReadingPlugin.tsx
+++ src/selfPacedReadingPlugin.tsx
@@ -19,7 +19,8 @@ const info = {
 
 type Info = typeof info;
 
-type TextLine = Array<string | [string, "normal" | "shown" | "hidden"]>;
+type DisplayType = "normal" | "shown" | "hidden";
+type TextLine = Array<string | [string, DisplayType]>;
 type Delimiter = string;
 type Concealer = "underline" | "dash";
 
@@ -34,6 +35,18 @@ class SelfPacedReadingPlugin implements JsPsychPlugin<Info> {
 
     trial(display_element: HTMLElement, trial: TrialType<Info>) {
         const root = createRoot(display_element);
+        const textLineSegmentSpan = (s: TextLine[0]) => {
+            const text: string = (typeof s === "string") ? s : s[0];
+            const displayType: DisplayType = (typeof s === "string") ? "normal" : s[1];
+            switch (displayType) {
+                case "normal":
+                    return <span className="textline-segment" data-displayText={text}></span>;
+                case "shown":
+                    return <span className="textline-segment-shown" data-displayText={text}></span>;
+                case "hidden":
+                    return <span className="textline-segment-hidden" data-displayText={text}></span>;
+            }
+        }
         root.render([] /* 20231213 備忘: ここに <span></span> みたいなのの配列を指定できる */);
     }
 }
