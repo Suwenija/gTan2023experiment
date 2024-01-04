e54be58bad883cc40297706450a96416e44c946b
janApupa <aloha.miyashita@gmail.com>
2024-01-04 13:07:33 +0900

    SPR プラグインの完成，実験本体の tsx の開発進める
    Author:    janApupa <aloha.miyashita@gmail.com>

--- src/experiment.tsx
+++ src/experiment.tsx
@@ -12,8 +12,9 @@ import "../styles/main.scss";
 import FullscreenPlugin from "@jspsych/plugin-fullscreen";
 import HtmlButtonResponsePlugin from "@jspsych/plugin-html-button-response";
 import SurveyHtmlFormPlugin from "@jspsych/plugin-survey-html-form";
+import { SelfPacedReadingPlugin } from "./selfPacedReadingPlugin";
 import { initJsPsych } from "jspsych";
-import React, { ReactComponentElement, useState } from "react";
+import React, { DOMElement, FC, useRef, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { renderToString } from "react-dom/server";
 
@@ -43,7 +44,7 @@ export async function run({ assetPaths, input = {}, environment, title, version 
         stimulus: renderToString(
             <div>
                 <p>実験に際して、以下のことに同意いただける場合は、ボタンを押して先にお進みください。</p>
-                <ul className="text-left">
+                <ul className="welcoming-notes">
                     <li>本実験は、横浜翠嵐高校 G探究 2年 言語学班が行っております。</li>
                     <li>得たデータは、研究成果の一部として発表される可能性があります。</li>
                     <li>個人情報は収集しません。（実験者の意図によらず入力された場合でも、無関係の第三者に公開することはありません。）</li>
@@ -66,32 +67,44 @@ export async function run({ assetPaths, input = {}, environment, title, version 
     // The main phase of the experiment here
 
     // Metadata survey
-    const gradeRadioInput = (num: string) => {
-        const id = "grade-" + num + "th";
-        return <>
-            <input type="radio" id={id} name="grade" value={num + "th"} required />
-            <label htmlFor={id}>翠嵐 {num} 期生</label>
-        </>
+    const GradeOption: FC<{num: string}> = ({num}) => {
+        return <option value={num + "th"}>翠嵐 {num} 期生</option>
     }
-    const radioAnotherInput =
-        (nameName: string, placeholder: string, label = "その他", subInputSuffix = "", freeInputType: "text" | "number" = "text") => {
-            const [freeInputDisabled, setFreeInputDisabled] = useState(true);
-            const [freeInputRequired, setFreeInputRequired] = useState(false);
-            const freeInput =
-                <input type={freeInputType} id={nameName+"-free"} name={nameName+"-free"} placeholder={placeholder} disabled={freeInputDisabled} required={freeInputRequired} />;
-            const toggleFree = (checked: boolean) => {
-                setFreeInputDisabled(!checked);
-                setFreeInputRequired(checked);
+    const SelectWithFreeInput: FC<{
+        children?,
+        nameName: string,
+        placeholder?: string,
+        freeInputPlaceholder?: string,
+        freeInputValue?: string,
+        label?: string,
+        freeInputSuffix?: string,
+        freeInputType?: "text" | "number"
+    }> = ({children, nameName, placeholder = "--選択--", freeInputPlaceholder, freeInputValue, label = "その他", freeInputSuffix = "", freeInputType = "text"}) => {
+        const valueFree = "FREE_DESCRIPTION";
+        let [freeInputDisabled, setFreeInputDisabled] = useState(true);
+        let [freeInputRequired, setFreeInputRequired] = useState(false);
+        const toggleFree = async (value: string) => {
+            await setFreeInputDisabled(!(value === valueFree));
+            await setFreeInputRequired(value === valueFree);
+            if (value === valueFree) {
+                document.getElementById(nameName+"-free")?.focus();
             }
-            const id = nameName+"-another";
-            const res =
-                <>
-                    <input type="radio" id={id} name={nameName} value="another" onChange={(e) => toggleFree(e.target.checked)} required />
-                    <label htmlFor={id}>{label}:</label>
-                    <span className="q-free">{freeInput}{subInputSuffix}</span>
-                </>
-            return res;
         };
+        const res =
+            <>
+                <select name={nameName} onChange={(e) => toggleFree(e.target.value)} required>
+                    <option value="" hidden>{placeholder}</option>
+                    {children}
+                    <option value={valueFree}>{label}</option>
+                </select>
+                <div className="q-free">
+                    自由記述：
+                    <input type={freeInputType} id={nameName+"-free"} name={nameName+"-free"} value={freeInputValue} placeholder={freeInputPlaceholder} disabled={freeInputDisabled} required={freeInputRequired} />
+                    {freeInputSuffix}
+                </div>
+            </>
+        return res;
+    };
     timeline.push({
         type: SurveyHtmlFormPlugin,
         html: renderToString(
@@ -109,28 +122,64 @@ export async function run({ assetPaths, input = {}, environment, title, version 
                     <p>最後にアンケートにご協力ください：</p>
                     <fieldset id="q-grade">
                         <legend>Q1. あなたが当てはまるものをお選びください。</legend>
-                        {gradeRadioInput("76")}
-                        {gradeRadioInput("77")}
-                        {gradeRadioInput("78")}
-                        {radioAnotherInput("grade", "所属等")}
+                        <SelectWithFreeInput nameName="grade" freeInputPlaceholder="所属等">
+                            <GradeOption num="76"/>
+                            <GradeOption num="77"/>
+                            <GradeOption num="78"/>
+                        </SelectWithFreeInput>
                     </fieldset>
                     <fieldset id="q-mother-tongue">
                         <legend>Q2. あなたの母語は？</legend>
-                        <input type="radio" id="mother-tongue-ja" name="mother-tongue" value="ja" required />
-                        <label htmlFor="mother-tongue-ja">日本語</label>
-                        {radioAnotherInput("mother-tongue", "○○語")}
+                        <SelectWithFreeInput nameName="mother-tongue" freeInputPlaceholder="○○語">
+                            <option value="ja">日本語</option>
+                        </SelectWithFreeInput>
                     </fieldset>
                     <fieldset id="q-is-first">
                         <legend>Q3. 今回参加されたのは初めてですか？</legend>
-                        <input type="radio" id="is-first-yes" name="is-first" value="first" required />
-                        <label htmlFor="is-first-yes">初めて</label>
-                        {radioAnotherInput("is-first", "2", "2 回目以上", " 回目", "number")}
+                        <SelectWithFreeInput nameName="is-first" freeInputValue="2" label="2 回目以上" freeInputSuffix=" 回目" freeInputType="number">
+                            <option value="first">初めて</option>
+                        </SelectWithFreeInput>
                     </fieldset>
                 </>
             )
         }
     });
 
+    timeline.push({
+        type: SelfPacedReadingPlugin,
+        textline: [
+            "いつの",
+            "世も、",
+            "イデオロギーによる",
+            "対立は",
+            "社会に",
+            "分断を",
+            "引き起こす",
+            "一つの",
+            "原因と",
+            "なっている。",
+            "冷戦期において",
+            "資本主義と",
+            "社会主義の",
+            "両陣営を",
+            "隔てた",
+            "溝は、",
+            "その",
+            "最たる",
+            "例だと",
+            "いえるだろう。",
+            "為道論を",
+            "めぐる",
+            "大小",
+            "さまざまな",
+            "闘争に",
+            "直面したとき、",
+            "いま",
+            "我々には",
+            "何が",
+            "できるのだろうか。",
+        ]
+    })
     await jsPsych.run(timeline);
 
     // Return the jsPsych instance so jsPsych Builder can access the experiment results (remove this
